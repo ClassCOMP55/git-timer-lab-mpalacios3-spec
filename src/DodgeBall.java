aa314b028e5c8383570e5b36b0d258a405910f14
Maxwell Palacios <m_palacios3@u.pacific.edu>
2026-02-22 04:40:21 -0800

    added wiggly enemy movement

--- src/DodgeBall.java
+++ src/DodgeBall.java
@@ -46,6 +46,7 @@ public class DodgeBall extends GraphicsProgram implements ActionListener {
 			addAnEnemy();
 		}
 		moveAllBallsOnce();
+		moveAllEnemiesOnce();
 	}
 	
 	public void mousePressed(MouseEvent e) {
@@ -87,6 +88,12 @@ public class DodgeBall extends GraphicsProgram implements ActionListener {
 	private void moveAllBallsOnce() {
 		for(GOval ball:balls) {
 			ball.move(SPEED, 0);
+		}
+	}
+	
+	private void moveAllEnemiesOnce() {
+		for (GRect enemy : enemies) {
+			enemy.move(0, rgen.nextInt(-2, 2));
 		}
 	}
 	
