fbcbc56e9a4de6e4ac648faea4e16872a99a46c4
Maxwell Palacios <m_palacios3@u.pacific.edu>
2026-02-22 04:14:57 -0800

    all balls move across the screen

--- src/BallLauncher.java
+++ src/BallLauncher.java
@@ -5,10 +5,13 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import javax.swing.*;
 
-public class BallLauncher extends GraphicsProgram{
+public class BallLauncher extends GraphicsProgram {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int SIZE = 25;
+	private ArrayList<GOval> balls;
+	final int MS = 50;
+	final int SPEED = 2;
 	
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -16,7 +19,10 @@ public class BallLauncher extends GraphicsProgram{
 	}
 	
 	public void run() {
+		Timer t = new Timer(1000, this);
+		t.start();
 		addMouseListeners();
+		
 	}
 	
 	public void mousePressed(MouseEvent e) {
@@ -28,7 +34,14 @@ public class BallLauncher extends GraphicsProgram{
 		GOval temp = new GOval(x-SIZE/2, y-SIZE/2, SIZE, SIZE);
 		temp.setColor(Color.RED);
 		temp.setFilled(true);
+		balls.add(temp);
 		return temp;
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		for (GOval x : balls) {
+			x.move(SPEED, 0);
+		}
 	}
 	
 	public static void main(String[] args) {
--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,14 @@
 import acm.graphics.*;
+import javax.swing.*;
 import acm.program.*;
+import java.awt.event.*;
 
-public class MyFirstTimer extends GraphicsProgram {
+public class MyFirstTimer extends GraphicsProgram implements ActionListener{
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	private int numTimes;
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -15,6 +18,19 @@ public class MyFirstTimer extends GraphicsProgram {
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		Timer t = new Timer(1000, this);
+		numTimes = 0;
+		t.setInitialDelay(3000);
+		t.start();
+		pause(10000);
+		t.stop();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		//TODO add what we want to do every second
+		myLabel.move(5, 0);
+		numTimes++;
+		myLabel.setLabel("times called?"  + numTimes);
 	}
 	
 	public static void main(String[] args) {
